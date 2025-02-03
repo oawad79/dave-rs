ee5c4c103e03fa72646e62ed63ed8004d225b24e
oawad79 <oawad79@gmail.com>
2025-02-02 18:33:45 -0600

    changes

--- src/game/mod.rs
+++ src/game/mod.rs
@@ -4,30 +4,17 @@ use macroquad_tiled::{load_map, Map};
 
 use crate::{player::Player, resources::Resources, Scene};
 
-#[derive(Debug)]
-struct Diamond {
-    world_x: f32,
-    world_y: f32,
-    world_w: f32,
-    world_h: f32,
-    tile_x: u32,
-    tile_y: u32,
-    tile_w: u32,
-    tile_h: u32,
-    name: String,
-    collected: bool,
-}
-
 struct GameObject {
     world_x: f32,
     world_y: f32,
     name: String,
+    collected: Option<bool>,
 }
 
 pub struct Game {
     world: World,
     player: Player,
-    diamonds: Vec<Diamond>,
+    collectibles: Vec<GameObject>,
     door: GameObject,
     trophy: GameObject,
     game_won: bool,
@@ -71,23 +58,17 @@ impl Game {
         let player = Player::new(actor);
 
         let objects_layer = tiled_map.layers.get("collectibles").unwrap();
-        let diamonds = objects_layer
+        let collectibles = objects_layer
             .objects
             .iter()
             .map(|entry| 
-                Diamond {
+                GameObject {
                     world_x: entry.world_x,
                     world_y: entry.world_y,
-                    world_w: entry.world_w,
-                    world_h: entry.world_h,
-                    tile_x: entry.tile_x,
-                    tile_y: entry.tile_y,
-                    tile_w: entry.tile_w,
-                    tile_h: entry.tile_h,
                     name: entry.name.clone(),
-                    collected: false,
+                    collected: None,
                 }
-            ).collect::<Vec<Diamond>>();
+            ).collect::<Vec<GameObject>>();
         
         let door = tiled_map.layers.get("door").unwrap().objects.first().unwrap();
 
@@ -95,20 +76,21 @@ impl Game {
             world_x: door.world_x,
             world_y: door.world_y,
             name: door.name.clone(),
+            collected: None,
         };
         
         let cup = tiled_map.layers.get("cup").unwrap().objects.first().unwrap();    
         let trophy: GameObject = GameObject {
             world_x: cup.world_x,
             world_y: cup.world_y,
-            name: cup.name.clone()
-            
+            name: cup.name.clone(),
+            collected: None,
         };
 
         Game {
             world,
             player,
-            diamonds,
+            collectibles,
             door,
             trophy,
             game_won: false,
@@ -124,7 +106,7 @@ impl Scene for Game {
         let pos = self.world.actor_pos(self.player.collider);
 
         // Check for collision between player and diamonds
-        for diamond in self.diamonds.iter_mut() {
+        for diamond in self.collectibles.iter_mut() {
             let diamond_rect = Rect::new(
                 diamond.world_x,
                 diamond.world_y - 32.0,
@@ -133,12 +115,12 @@ impl Scene for Game {
             );
 
             if self.player.overlaps(pos, &diamond_rect) {
-                diamond.collected = true;
+                diamond.collected = Option::Some(true);
                 play_sound_once(&resources.sound_collect);
             }
         }
 
-        self.diamonds.retain(|diamond| !diamond.collected);
+        self.collectibles.retain(|diamond| !diamond.collected.unwrap_or(false));
 
         let pos = self.world.actor_pos(self.player.collider);
 
@@ -171,7 +153,7 @@ impl Scene for Game {
         self.tiled_map
             .draw_tiles("platform", Rect::new(0.0, 0.0, 608.0, 320.0), None);
 
-        for diamond in &self.diamonds {
+        for diamond in &self.collectibles {
             let x = if diamond.name == "ruby" {
                 0.0
             } else if diamond.name == "diamond" {
--- src/player.rs
+++ src/player.rs
@@ -3,7 +3,7 @@ use macroquad_platformer::{Actor, World};
 use macroquad::prelude::*;
 use macroquad_tiled::Map;
 
-use crate::{game, Resources};
+use crate::Resources;
 
 const GRAVITY: f32 = 500.0;
 const JUMP_VELOCITY: f32 = -260.0;
