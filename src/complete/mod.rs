d6775ac68e938a6adf2fbd84f302a611ff03694c
oawad79 <oawad79@gmail.com>
2025-04-02 10:50:12 -0500

    clippy

--- src/complete/mod.rs
+++ src/complete/mod.rs
@@ -1,7 +1,26 @@
-use macroquad::{color::WHITE, input::{is_key_down, KeyCode}, math::Rect, prelude::collections::storage, text::{draw_text_ex, TextParams}};
-use macroquad_tiled::{load_map, Map};
+use macroquad::{
+    color::WHITE,
+    input::{
+        KeyCode,
+        is_key_down,
+    },
+    math::Rect,
+    prelude::collections::storage,
+    text::{
+        TextParams,
+        draw_text_ex,
+    },
+};
+use macroquad_tiled::{
+    Map,
+    load_map,
+};
 
-use crate::{resources::Resources, Scene, SceneChange};
+use crate::{
+    Scene,
+    SceneChange,
+    resources::Resources,
+};
 
 pub struct Complete {
     tiled_map: Map,
@@ -13,16 +32,15 @@ impl Complete {
 
         let tiled_map = load_map(
             &resources.done_map_json,
-            &[
-                ("images/collectibles.png", resources.get_texture("collectibles").clone())
-            ],
+            &[(
+                "images/collectibles.png",
+                resources.get_texture("collectibles").clone(),
+            )],
             &[],
         )
         .unwrap();
 
-        Self {
-            tiled_map
-        }
+        Self { tiled_map }
     }
 
     fn draw_texture(text: &str, x: f32, y: f32, resources: &Resources) {
@@ -31,9 +49,9 @@ impl Complete {
             x,
             y,
             TextParams {
-                font: Some(&resources.font), 
-                font_size: 25,     
-                color: WHITE,  
+                font: Some(&resources.font),
+                font_size: 25,
+                color: WHITE,
                 ..Default::default()
             },
         );
@@ -42,7 +60,6 @@ impl Complete {
 
 impl Scene for Complete {
     fn update(&mut self) -> Option<SceneChange> {
-
         if is_key_down(KeyCode::Enter) {
             return Some(SceneChange::EntryScreen);
         }
@@ -52,72 +69,52 @@ impl Scene for Complete {
 
     fn draw(&mut self) {
         let resources = storage::get::<Resources>();
-        
+
         self.tiled_map
             .draw_tiles("platform", Rect::new(0.0, 0.0, 608.0, 384.0), None);
 
-        Complete::draw_texture("CONGRATULATIONS !", 230.0, 70.0, &resources);
-        Complete::draw_texture("YOU MADE IT THROUGH ALL THE PERILOUS AREAS IN ",
+        Self::draw_texture("CONGRATULATIONS !", 230.0, 70.0, &resources);
+        Self::draw_texture(
+            "YOU MADE IT THROUGH ALL THE PERILOUS AREAS IN ",
             50.0,
             110.0,
-            &resources
+            &resources,
         );
 
-        Complete::draw_texture(
-            "THE DAVE THE DUCK ADVENTURE !",
-            50.0,
-            150.0,
-            &resources
-        );
+        Self::draw_texture("THE DAVE THE DUCK ADVENTURE !", 50.0, 150.0, &resources);
 
-        Complete::draw_texture(
-            "CLYDE'S HIDEOUT!",
-            50.0,
-            150.0,
-            &resources
-        );
-         
-        Complete::draw_texture(
+        Self::draw_texture("CLYDE'S HIDEOUT!", 50.0, 150.0, &resources);
+
+        Self::draw_texture(
             "VERY GOOD WORK! DID YOU FIND THE 4 WARP ZONES ? ",
             50.0,
             190.0,
-            &resources
-        );    
+            &resources,
+        );
 
-        Complete::draw_texture(
+        Self::draw_texture(
             "THEY ARE LOCATED ON LEVELS 5, 8, 9 AND 10. JUST ",
             50.0,
-            210.0,  
-            &resources
-        );    
+            210.0,
+            &resources,
+        );
 
-        Complete::draw_texture(
+        Self::draw_texture(
             "JUMP OFF THE TOP OF THE SCREEN AT THE EXTREME ",
             50.0,
             230.0,
-            &resources
-        );    
+            &resources,
+        );
 
-        Complete::draw_texture(
+        Self::draw_texture(
             "LEFT OR RIGHT EDGE OF THE WORLD AND VIOLA ! YOU'RE ",
             50.0,
             250.0,
-            &resources
-        );    
+            &resources,
+        );
 
-        Complete::draw_texture(
-            "THERE ! ",
-            50.0,
-            270.0,
-            &resources
-        );    
+        Self::draw_texture("THERE ! ", 50.0, 270.0, &resources);
 
-        Complete::draw_texture(
-            "PRESS ENTER ",
-            240.0,
-            320.0,
-            &resources
-        );    
-
-    }    
+        Self::draw_texture("PRESS ENTER ", 240.0, 320.0, &resources);
+    }
 }
